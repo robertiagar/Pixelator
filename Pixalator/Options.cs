4e37cf95062ca6e6c03bb5f03b7fdb05922a7586
Robert Iagar <robert.iagar@gmail.com>
2015-08-28 17:12:15 +0300

    initial commit

---
+++ Pixalator/Options.cs
@@ -0,0 +1,24 @@
+﻿using CommandLine;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pixalator
+{
+	public class Options
+	{
+		[Option('v', "verbose", HelpText = "Prints all messages to standard output.")]
+		public bool Verbose { get; set; }
+
+		[Option('i', "imaage", HelpText = "Path to the original image.", Required = true)]
+		public string ImagePath { get; set; }
+
+		[Option('r', "result", HelpText = "Path to the resulting image.", Required = true)]
+		public string ResultPath { get; set; }
+
+		[Option('s', "size", HelpText = "Pixel size.", Required = true)]
+		public int PixelSize { get; set; }
+	}
+}
